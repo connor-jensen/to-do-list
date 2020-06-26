7202c5c5523e5f699652848c079abf4633b70783
Connor Jensen <connor.jensen@live.com>
2020-06-26 12:46:24 -0600

    initial set up and documented outline in comments

---
+++ src/index.js
@@ -0,0 +1,23 @@
+console.log('test');
+/*
+to-do object
+   title
+   description
+   dueDate
+   priority
+   notes
+   completed (boolean)
+
+project object
+   array of to-do objects
+
+render
+   render project
+   render to-dos from project
+   render to-do details
+   change to-do / project display properties
+      for example, strikethrough when completed
+
+controller
+   application logic
+*/
